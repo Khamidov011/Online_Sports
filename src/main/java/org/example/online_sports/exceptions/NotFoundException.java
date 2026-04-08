578a3434a8da899fd7c6982f53e880724bd06ad2
pilot <diyorbeklolayev7@gmail.com>
2026-04-08 14:56:34 +0500

    diyorbek

--- src/main/java/org/example/online_sports/exceptions/NotFoundException.java
+++ src/main/java/org/example/online_sports/exceptions/NotFoundException.java
@@ -1,6 +1,5 @@
 package org.example.online_sports.exceptions;
 
-import org.example.online_sports.payload.ApiResponse;
 
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
